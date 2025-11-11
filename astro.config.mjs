87e1a4bf8e46563b31c3266845fdf7c8f4126582
efahssi <elias@fahssi.ca>
2025-11-11 13:55:53 -0700

    config and deploy actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro.vercel.app",
+  site: "https://efahssi.github.io",
+  base: "/website",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
