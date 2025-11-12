540c7b6b2221ecefe771255034fa3c452f2b15a3
efahssi <elias@fahssi.ca>
2025-11-11 17:14:08 -0700

    update config url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://efahssi.github.io",
+  site: "https://elias.fahssi.ca",
   base: "/website",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
